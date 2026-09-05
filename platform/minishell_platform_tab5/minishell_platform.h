d5819e3ca0fb6c08d8b52eb84c7602b06e143630
wcheng95 <66143141+wcheng95@users.noreply.github.com>
2026-09-04 21:59:42 -0700

    task0: add Tab5 platform storage layer

---
+++ platform/minishell_platform_tab5/minishell_platform.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int minishell_platform_init(void);
+bool minishell_platform_sd_ready(void);
+const char *minishell_platform_sd_status(void);
+const char *minishell_platform_name(void);
+
+#ifdef __cplusplus
+}
+#endif
