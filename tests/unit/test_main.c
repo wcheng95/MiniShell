c412868f15520cfe6738c5cd05ee177bfebaf2b5
wcheng95 <66143141+wcheng95@users.noreply.github.com>
2026-09-05 15:18:43 -0700

    Add cp copy test group

--- tests/unit/test_main.c
+++ tests/unit/test_main.c
@@ -15,6 +15,7 @@ static const test_case_t tests[] = {
     {"transfer", test_transfer},
     {"power", test_power},
     {"nano_buffer", test_nano_buffer},
+    {"cp_copy", test_cp_copy},
 };
 
 int main(int argc, char **argv)
