d16bb113b5dd54011e27dcac38ea7e683c16a712
wcheng95 <66143141+wcheng95@users.noreply.github.com>
2026-09-06 16:46:14 -0700

    Expose private platform name for status

--- core/platform_backend.h
+++ core/platform_backend.h
@@ -9,6 +9,7 @@ typedef void (*minishell_app_emit_fn)(const char *name, void *ctx);
 int minishell_platform_init(void);
 void minishell_platform_shutdown(void);
 void minishell_platform_write(const char *text);
+const char *minishell_platform_name(void);
 
 const minishell_services_port_t *minishell_platform_services_port(void);
 
