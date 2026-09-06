07b2c2a565fa4f819939d6e118b74734eb2be674
wcheng95 <66143141+wcheng95@users.noreply.github.com>
2026-09-06 16:06:01 -0700

    Expose MiniShell service backend port

--- core/platform_backend.h
+++ core/platform_backend.h
@@ -2,11 +2,15 @@
 
 #include <stddef.h>
 
+#include "minishell_services.h"
+
 typedef void (*minishell_app_emit_fn)(const char *name, void *ctx);
 
 int minishell_platform_init(void);
 void minishell_platform_shutdown(void);
 void minishell_platform_write(const char *text);
+
+const minishell_services_port_t *minishell_platform_services_port(void);
 
 int minishell_platform_apps_list(minishell_app_emit_fn emit, void *ctx);
 int minishell_platform_app_run(const char *name, int argc, char **argv);
