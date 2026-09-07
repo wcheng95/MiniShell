7e7db103e06af649d8f09215fcc0fcb183fa7f32
wcheng95 <66143141+wcheng95@users.noreply.github.com>
2026-09-06 18:36:15 -0700

    chore: remove legacy Tab5 tests docs and tools

--- tests/unit/test_support.h
+++ tests/unit/test_support.h
@@ -95,7 +95,5 @@ bool test_filesystem(void);
 bool test_time_location(void);
 bool test_display(void);
 bool test_input(void);
-bool test_transfer(void);
-bool test_power(void);
 bool test_nano_buffer(void);
 bool test_cp_copy(void);
