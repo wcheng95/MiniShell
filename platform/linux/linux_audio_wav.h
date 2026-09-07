52638cb7915f704990c6fb8479e04c54042f9ba0
wcheng95 <66143141+wcheng95@users.noreply.github.com>
2026-09-07 16:35:02 -0700

    audio: simplify Linux WAV provider interface

--- platform/linux/linux_audio_wav.h
+++ platform/linux/linux_audio_wav.h
@@ -2,5 +2,4 @@
 
 #include "minishell_services.h"
 
-void linux_audio_wav_configure(minishell_services_port_t *port, const char *root_dir);
-void linux_audio_wav_shutdown(void);
+void linux_audio_wav_configure(minishell_services_port_t *port);
