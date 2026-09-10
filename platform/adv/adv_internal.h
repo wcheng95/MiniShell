3be6ad1ebb6465294cad89fce0715f50bc8d3073
wcheng95 <66143141+wcheng95@users.noreply.github.com>
2026-09-09 22:20:29 -0700

    ADV: declare WAV Audio provider

--- platform/adv/adv_internal.h
+++ platform/adv/adv_internal.h
@@ -44,6 +44,7 @@ void adv_filesystem_shutdown(void);
 bool adv_filesystem_flash_ready(void);
 bool adv_filesystem_sd_ready(void);
 void adv_filesystem_configure(minishell_services_port_t *port);
+void adv_audio_wav_configure(minishell_services_port_t *port);
 void adv_time_location_configure(minishell_services_port_t *port);
 
 uint64_t adv_monotonic_us(void *ctx);
