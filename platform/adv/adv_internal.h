7bd85039c6cca8b4d84049f5f9b4853e2e0a7188
wcheng95 <66143141+wcheng95@users.noreply.github.com>
2026-09-10 22:42:24 -0700

    K5: expose ADV speaker provider internally

--- platform/adv/adv_internal.h
+++ platform/adv/adv_internal.h
@@ -45,6 +45,7 @@ bool adv_filesystem_flash_ready(void);
 bool adv_filesystem_sd_ready(void);
 void adv_filesystem_configure(minishell_services_port_t *port);
 void adv_audio_wav_configure(minishell_services_port_t *port);
+void adv_audio_speaker_configure(minishell_services_port_t *port);
 void adv_time_location_configure(minishell_services_port_t *port);
 void adv_digital_io_configure(minishell_services_port_t *port);
 
